e6d37fd7898036d61727e9dc39ad9447a9873dc0
SMoore3773 <Smooresculpture@gmail.com>
2020-08-01 13:35:45 -0400

    names complete

--- names/names.py
+++ names/names.py
@@ -13,10 +13,31 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+
+for name in names_1:
+    if name in names_2:
+        duplicates.append(name)
+# duplicates = map(lambda name: names_1.remove(name) if name in names_1 else None, names_2)
+
+# print(duplicates)
+
+# set_1 = set()
+# for name_2 in names_2:
+#     names_1.append(name_2)
+# for name in names_1:
+#     if name in set_1:
+#         duplicates.append(name)
+#     else:
+#         set_1.add(name)
+
+
+##### rumtime for this is O(n^2)#####
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
